e33d6bb15f0f14af6b7ba1cae35e6633f4b50ee6
Ritesh Jangra <riteshk2874@gmail.com>
2021-02-07 23:55:07 +0530

    edit1.0

---
+++ app.js
@@ -0,0 +1,25 @@
+$(".head1").click(function () {
+  $(".head1").toggleClass("clicked");
+  $(".qry1").toggle("slide");
+  $("img.arrow1").toggleClass("rotate");
+})
+$(".head2").click(function () {
+  $(".head2").toggleClass("clicked");
+  $(".qry2").toggle("slide");
+  $("img.arrow2").toggleClass("rotate");
+})
+$(".head3").click(function () {
+  $(".head3").toggleClass("clicked");
+  $(".qry3").toggle("slide");
+  $("img.arrow3").toggleClass("rotate");
+})
+$(".head4").click(function () {
+  $(".head4").toggleClass("clicked");
+  $(".qry4").toggle("slide");
+  $("img.arrow4").toggleClass("rotate");
+})
+$(".head5").click(function () {
+  $(".head5").toggleClass("clicked");
+  $(".qry5").toggle("slide");
+  $("img.arrow5").toggleClass("rotate");
+})
